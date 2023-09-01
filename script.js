ecc562133cc67309ab591396eb2542b7319060be
youssefgamiily <youssefgamiily@gmail.com>
2023-09-01 12:52:44 +0300

    4 tests passed

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 console.log("hello world")
 capitalize = (word) => {
-    let firstLetter = word[0].toUpperCase()
-    let newWord = ""
-    newWord = newWord + firstLetter
-    for (let letter of word) {
-        if (!(letter === `${word[0]}`)) {
-            newWord = newWord + letter
+    console.log(typeof word)
+    if(typeof word == "string" ) {
+        console.log("in Line 5")
+        let firstLetter = word[0].toUpperCase()
+        let newWord = ""
+        newWord = newWord + firstLetter
+        for (let letter of word) {
+            if (!(letter === `${word[0]}`)) {
+                newWord = newWord + letter
+            }
         }
+        return newWord
     }
-    return newWord
+    if (typeof word === 'object') return -5
+    console.log("reached EOF")
+    return word
 }
 
 reverseString = (word) => {
-    let reverseWord = []
-    let count = 0
-    for (let i=word.length-1; i>-1; i--) {
-        reverseWord[count] = word[i]
-        count ++ 
+    if (typeof word == "number") word = String(word)
+    if (typeof word == "string"){
+        let reverseWord = []
+        let count = 0
+        for (let i=word.length-1; i>-1; i--) {
+            reverseWord[count] = word[i]
+            count ++ 
+        }
+        return reverseWord.toString().replaceAll(",","")
     }
-    return reverseWord.toString().replaceAll(",","")
 }
 
 class Calculator {
@@ -36,4 +46,4 @@ class Calculator {
     }
 }
 
-module.export = {capitalze, reverseString}
+module.exports = {capitalize, reverseString}
--- test/testing.test.js
+++ test/testing.test.js
@@ -1,16 +1,14 @@
-const capitalize = require("../script.js")
-const reverseString = require("../script.js")
-it ('Not given a string format', () => {
-    expect(capitalize(hossam)).toBe("Hossam")
-})
+// test.js
+const script = require("../script.js");
+console.log(script)
+const capitalize = script.capitalize;
+const reverseString = script.reverseString;
+
 it ('Not given a text of any format', () => {
-    expect(capitalize(1234)).toBe("1234")
+    expect(capitalize(1234)).toBe(1234)
 })
 it ('not Reversing string', () => {
     expect(reverseString("Joe")).toBe("eoJ")
-})
-it ('not given a string', () => {
-    expect(reverseString(Joe)).toBe("eoJ")
 })
 it ('not given a string of any format', () => {
     expect(reverseString(2563)).toBe("3652")
